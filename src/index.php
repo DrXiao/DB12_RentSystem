43a9f78a62731d133102e526793ed937de7a1a0c
Dr.Xiao <zxc71230@gmail.com>
2020-11-22 11:43:01 +0800

    first commit

---
+++ src/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+
+
+<head>
+    <meta charset="utf-8">
+    <style>
+        <?php
+        include(dirname(__DIR__)."/static/css/test.css");
+        ?>
+    </style>
+    <title>My web</title>
+</head>
+
+
+<body>
+
+    <h1><?php echo dirname(__DIR__); ?></h1>
+</body>
+
+
+
+</html>
