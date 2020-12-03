6b99d425777e95e9d4bb975660024f5505580a46
Dr.Xiao <zxc71230@gmail.com>
2020-12-03 22:39:41 +0800

    Adding database func

--- src/index.php
+++ src/index.php
@@ -1,12 +1,16 @@
 <?php
-require("../vendor/autoload.php");
+//require("../vendor/autoload.php");
+$host = "ec2-52-20-248-222.compute-1.amazonaws.com";
+$port = "5432";
+$dbname = "ded5rsvcdg3lgk";
+$userAccount = "bnyojzfovtnuth";
+$userPassword = "7c25dceccb34298445b4f983a04ca8a74d704a989f68faca7ca89e6793fc1b65";
+$DATABASE_URL = "postgres://$userAccount:$userPassword@$host:$port/$dbname";
+$db = pg_connect($DATABASE_URL);
 
-$db = pg_connect(getenv("DATABASE_URL"));
-
-if(!$db) {
+if (!$db) {
     echo "Error\n";
-}
-else {
+} else {
     echo "Successful\n";
 }
 
