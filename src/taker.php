4441419d0662cff6a901f2dcbedcfb40fd7141f5
sunkaii <kevin02180229@gmail.com>
2020-12-05 12:17:48 +0800

    taker.php

---
+++ src/taker.php
@@ -0,0 +1,46 @@
+<html>
+
+<head>
+<title>承辦人</title>
+</head>
+
+<body>
+
+<p align="left"><font size="6"face="微軟正黑體">承辦人</font></p>
+<hr>
+
+尚未核准的申請件數 : xx
+<p>日期查詢<input type=date></p>
+
+<form>
+<table border="1"width="30%">
+<tr>
+  <th >案件編號</th>
+  <th>申請日期</th>
+  <th>申請人</th>
+  <th></th>
+  <th>批准與否</th>
+</tr>
+
+<tr align="center">
+  <td>123456789</td>
+  <td>2020/11/28</td>
+  <td>hongbin</td>
+  <td><input type="submit"value="詳細資料"></td>
+  <td><input type="radio"name="check"value="yes">是
+      <input type="radio"name="check"value="no">否</td>
+</tr>
+
+<tr align="center">
+  <td>564654132</td>
+  <td>2020/11/29</td>
+  <td>bonghin</td>
+  <td><input type="submit"value="詳細資料"></td>
+  <td><input type="radio"name="check"value="yes">是
+      <input type="radio"name="check"value="no">否</td>
+</tr>
+</table>
+<p><input type="submit"value="確認送出"></p>
+</form>
+</body>
+</html>
