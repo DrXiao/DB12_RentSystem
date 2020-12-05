8dd50928d81168289d62d2dfee278c3d1598103a
Dewei-Hou <a1075543@mail.nuk.edu.tw>
2020-12-05 12:07:53 +0800

    adding applicant

---
+++ src/applicant.php
@@ -0,0 +1,66 @@
+<html>
+
+
+
+<head>
+
+    <meta charset="utf-8">
+    <title>高大露營烤肉區租借系統</title>
+
+</head>
+
+
+
+<body>
+
+高大露營烤肉區租借系統<br>
+<table border=1 width="100%">
+<tr>
+    <td align="center">申請人(單位):<input type=text maxLength="10" size="10"name="UserName"></td>
+    <td align="center">學號(校內學生借用):<input type=text maxlength="8" size="8" name="RegisterNum"></td>
+
+<tr>
+    <td align="center">電子信箱:<input type=text name="email"></td>
+    <td align="center">連絡電話(請務必填寫):<input type=text maxlength="10" size="10" name="Phone"></td>
+
+<tr>
+    <td align="center">收據繳款(人)(公司)名稱:<input type=text maxlength="10" size="10" name="Company"></td>
+    <td align="center">總人數：<input type=text maxlength="10" size="10" name="PNum"></td>
+
+<tr>
+    <td align="center">統一編號:<input type=text name="TaxNum"></td>
+    <td align="center">稅籍編號：<input type=text name="TaxID"></td>
+
+<tr>
+    <td align="center">地址:<input type=text name="address"></td>
+
+<tr>
+    <td align="center">借用時間及時段:<input type=date name="Dates"></td>
+
+<tr>
+    <td align="center">烤肉爐:<input type=text maxlength="2" size="2" name="BBQ">個</td>
+    <td align="center">借用時段<input type=text maxlength="2" size="2" name="SHr">:
+    <input type=text maxlength="2" size="2" name="SMin">~
+    <input type=text maxlength="2" size="2" name="EHr">:
+    <input type=text maxlength="2" size="2" name="EMin"></td>
+
+<tr>
+    <td align="center">營位:<input type=text maxlength="2" size="2" name="BBQ">個</td>
+
+<tr>
+    <td align="left">備註:</td>
+
+<tr>
+    <td align="left">1.1時段為4小時．若有特殊需求，請洽事務組 07-5919094。</td>
+
+<tr>
+    <td align="left">2.若是校外人士租借，汽車停車收費方式(請先自備場地繳費收據正本或影本，並於進校園時出示給警衛室)：將以日計費/每台車收30元。</td>
+
+<tr>
+<td align="center">
+<input value="申請"type="submit">
+<input value="清除"type="reset"></td>
+</tr>
+
+</body>
+</html>
