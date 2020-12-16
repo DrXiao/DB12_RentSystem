5453a69925ead69a15b52636668b75691ef81e58
Dr.Xiao <zxc71230@gmail.com>
2020-12-16 23:05:50 +0800

    Adding sign up functions

--- src/signUp.php
+++ src/signUp.php
@@ -1,4 +1,33 @@
 <!DOCTYPE html>
+<?php
+if ($_POST) {
+    $db = pg_connect(getenv("DATABASE_URL"));
+    // NULL => 空值
+
+    if (!$db) {
+        echo "Error<br>";
+    } else {
+        echo "Successful<br>";
+    }
+
+    $query = <<<EOF
+                Select Account, SU_Password, SU_Name From SystemUser;
+            EOF;
+
+    $returnTable = pg_query($db, $query);
+
+    if (!$returnTable) {
+        echo pg_last_error($db);
+        exit;
+    }
+
+    
+    pg_close($db);
+}
+
+?>
+
+
 <html>
 
 <head>
@@ -19,11 +48,11 @@
 <body>
     <div id="BasicForm">
         <h2>基本資料</h2>
-        <form action="signUp.php" method="post">
+        <form action="signUp.php" method="post" id="InfoForm" onsubmit="return checkPwd(this);">
             <table style="border:3px #000000 solid;">
                 <tr>
                     <td>帳號：<input type="text" name="account" maxlength="20"></td>
-                    <td>電話：<input type="text" name="phone" maxlength="10" pattern=""></td>
+                    <td>電話：<input type="text" name="phone" maxlength="10" pattern="[0-9]+"></td>
                     <td>名字：<input type="text" name="name"></td>
                     <td>地址：<input type="text" name="address"></td>
                 </tr>
@@ -37,22 +66,26 @@
                 <tr>
                     <td>
                         身份：
-                        <select id="ident" onchange="changeExtraForm()">
+                        <select id="ident" name="identity" onchange="changeExtraForm()">
                             <option value="00">校外人士</option>
                             <option value="01">學生</option>
+                            <option value="10">教職員</option>
                         </select>
                     </td>
-                </tr>
-                <tr id="Student" hidden>
-                    <td>學號：<input type="text" name="studentID" maxlength="8" pattern="[ABLM][0-9]{7}">
                 </tr>
                 <tr id="Outsider">
                     <td>統一編號：<input type="text" name="uniformNum" maxlength="8"></td>
                     <td>稅籍編號：<input type="text" name="taxSerialNum"></td>
                     <td>公司：<input type="text" name="company" maxlength="20"></td>
                 </tr>
+                <tr id="Student" hidden>
+                    <td>學號：<input type="text" name="studentID" maxlength="8" pattern="[ABLM][0-9]{7}"></td>
+                </tr>
+                <tr id="Staff" hidden>
+                    <td>教職員編號：<input type="text" name="staffID"></td>
+                </tr>
             </table>
-            <p><input type="submit" value="註冊"> 
+            <p><input type="submit" value="註冊" >
         </form>
     </div>
 </body>
