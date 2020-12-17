daf365e298d7bc4aa57ec367b0a3a010445a2949
Dr.Xiao <zxc71230@gmail.com>
2020-12-17 19:38:48 +0800

    Revising singUp page

--- src/index.php
+++ src/index.php
@@ -1,4 +1,11 @@
 <!DOCTYPE html>
+
+<?php
+    if($_POST) {
+        
+    }
+?>
+
 <html>
 
 <head>
@@ -14,7 +21,7 @@
 <body>
     <div id="Main">
         <h2>高大露營區租借系統</h2>
-        <form action="rent.php" method="post">
+        <form action="index.php" method="post">
             帳號：<input type="text" name="account" maxlength="20"></br>
             密碼：<input type="password" name="password" maxlength="30">
             <p><input type="submit" value="登入"> <input type="reset">
